883ce915c1c63f3014154e0dfc6d980de603b8d7
evelyn <wangyanyuan000@gmail.com>
2023-11-18 17:14:48 -0500

    feat: distance detection and countdown

--- frontend/src/pages/SummaryBoard.js
+++ frontend/src/pages/SummaryBoard.js
@@ -1,54 +1,23 @@
-
-
+import {useNavigate, useLocation} from "react-router-dom";
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
-function SummaryBoard({ timeCompleted }) {
+const SummaryBoard = () => {
+    let navigate = useNavigate();
+    let location = useLocation();
+    let remainingTime = location.state?.remaining_time;
+    const [completionTime, setCompletionTime] = useState(new Date().toLocaleTimeString());
 
-    // Format the total time as minutes:seconds
-    const formatTotalTime = (seconds) => {
-        const mins = Math.floor(seconds / 60);
-        const secs = seconds % 60;
-        return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
-    };
-
-    const closeModal = () => {
-        setShowModal(false);
+    const handleClose = () => {
+       navigate('/');
     };
 
     return (
         <div>
-            {showModal && (
-                <div className="modal">
-                    <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-                        <h2>Session Summary</h2>
-                        <p>Total Countdown Time: {formatTotalTime(timeCompleted)}</p>
-                        <Link to="/" style={buttonStyle}>
-                            Close
-                        </Link>
-                    </div>
-                </div>
-            )}
+            <h2>Task Completed!</h2>
+            <p>Time of Completion: {remainingTime}</p>
+            <button onClick={handleClose}>Close</button>
         </div>
     );
-}
-
-const buttonStyle = {
-    backgroundColor: '#FF69B4',
-    color: 'white',
-    border: 'none',
-    borderRadius: '20px',
-    padding: '10px 20px',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    cursor: 'pointer',
-    marginTop: '10px',
-    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
 };
 
 export default SummaryBoard;
-
-
