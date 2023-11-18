a15e21416472f7ab460b28c1f4c6196e53941bb5
Hanzi Li <hanzili>
2023-11-18 17:27:15 -0500

    feat: frontend backend integration

---
+++ frontend/src/components/LoginComponent/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(JSON.stringify({ email, password }))
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+      const data = await response.json();
+      console.log(data);
+      // Handle response or redirect user
+    } catch (error) {
+      console.error(error);
+      // Handle error
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        />
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+        />
+      </div>
+      <div>
+        <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RegisterComponent from '../components/RegisterComponent';
+import LoginComponent from '../components/LoginComponent';
 
 const SignUp = () => {
+    const [showLogin, setShowLogin] = useState(true);
+
     return (
-        <div>
-            <h1>Sign Up Successful</h1>
+        <div className="container mx-auto p-4">
+            {showLogin ? <LoginComponent /> : <RegisterComponent />}
+            <button
+                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setShowLogin(!showLogin)}
+            >
+                Switch to {showLogin ? 'Register' : 'Login'}
+            </button>
         </div>
     );
 };
