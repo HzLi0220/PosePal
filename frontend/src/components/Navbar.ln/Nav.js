5c1fb3951c4401ffd4887cebac6d04dc172a03e2
evelyn <wangyanyuan000@gmail.com>
2023-11-19 01:28:34 -0500

    corrected routing

--- frontend/src/components/Navbar.ln/Nav.js
+++ frontend/src/components/Navbar.ln/Nav.js
@@ -11,7 +11,7 @@ function Nav (){
     }
 
     return <div className='font-monomaniac font-bold w-full flex justify-evenly'>
-        <button onClick={handleHistory} className='text-3xl bg-blue border border-white px-7 py-4 rounded-xl text-white hover:bg-pink-500 transition-colors duration-300'>New Session</button>
+        <button onClick={handleNewSession} className='text-3xl bg-blue border border-white px-7 py-4 rounded-xl text-white hover:bg-pink-500 transition-colors duration-300'>New Session</button>
         <button onClick={handleHistory} className='text-3xl bg-blue border border-white px-14 py-3 rounded-xl text-white hover:bg-pink-500 transition-colors duration-300'>History</button>
     </div>
 }
